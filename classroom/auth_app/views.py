d7b0e36d16ba140abc571667045216f54ea9d9df
4S7VEN <157816271+4S7VEN@users.noreply.github.com>
2024-07-20 20:15:29 +0300

    Update views.py

--- classroom/auth_app/views.py
+++ classroom/auth_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth import login, logout as auth_logout, authenticate
 from .forms import SignUpForm
 from django.http import HttpResponse
 from django.template import loader
@@ -10,47 +10,47 @@ def main(request):
     template = loader.get_template('index.html')
     return HttpResponse(template.render())
 
+
 def sign_up(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('login/')  # Redirige vers une page d'accueil ou une page spécifique
+            return redirect('login')  # Redirects to a login page or specific page
     else:
         form = SignUpForm()
     return render(request, 'signup_form.html', {'form': form})
 
+
 @login_required
 def loginType(request):
-    form = SignUpForm(request.POST)
-    user = form.save()
-    login(request, user)
-             # Détermine si l'utilisateur est un étudiant ou un enseignant
-    if user.user_type == 2 :
-        return redirect('student_home')  # Assurez-vous que 'student_home' est défini dans urls.py
-    elif user.user_type == 1 :
-        return redirect('teacher_home')  # Assurez-vous que 'teacher_home' est défini dans urls.py
+    user = request.user
+    # Determine if the user is a student or teacher
+    if user.user_type == 2:
+        return redirect('student_home')  # Ensure 'student_home' is defined in urls.py
+    elif user.user_type == 1:
+        return redirect('teacher_home')  # Ensure 'teacher_home' is defined in urls.py
     else:
-        return redirect('login/')
+        return redirect('login')
+
 
 @login_required
 def home(request):
     template = loader.get_template('home.html')
-    return HttpResponse(template.render())
+    return HttpResponse(template.render(request=request))
 
 
 def logout(request):
-    logout(request)
+    auth_logout(request)
     return redirect('login')
 
+
+@login_required
 def student_home(request):
     return render(request, 'student_dashboard.html')
 
+
+@login_required
 def teacher_home(request):
     return render(request, 'teacher_dashboard.html')
-
-
-
-
-
